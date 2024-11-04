9b13a05c1c63b40f14eddddb3df66bcce46de844
ephrem <ephremmamo555@gmail.com>
2024-11-04 11:04:27 +0300

    add: add user and product models

--- server/controller/welcome_controller.js
+++ server/controller/welcome_controller.js
@@ -1,3 +1,3 @@
-export function welcome(req, res){
-    res.json({'message': "Welcome to delta eCommerce"})
+export function welcome(req, res) {
+  res.json({ message: 'Welcome to delta eCommerce' });
 }
